238080dd110689efa487a843afaa7fd9ca11fe69
Adam Wynne <adam.wynne@us.bosch.com>
2015-11-18 09:28:40 -0500

    created launcher script for osx

--- bindings/java/zyre-jni/demo-native/env_osx.sh
+++ bindings/java/zyre-jni/demo-native/env_osx.sh
@@ -3,5 +3,6 @@
 # set directory where this script lives
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export DYLD_LIBRARY_PATH="$DIR/../native/osx:$DIR/../tmp/local/lib"
+#export DYLD_LIBRARY_PATH="$DIR/../native/osx:$DIR/../tmp/local/lib"
+export JAVA_LIBRARY_PATH="$DIR/../native/osx:$DIR/../tmp/local/lib"
 
